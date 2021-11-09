f559eb543c6f123db8df30c6551315bc2a81f64b
FFF2832 <90552202+FFF2832@users.noreply.github.com>
2021-11-09 11:25:17 +0800

    first commit

---
+++ toneMelody/toneMelody.ino
@@ -0,0 +1,48 @@
+#include "pitches.h"
+
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+
+int tonePin = 12;
+
+////int melody[] = {
+//  NOTE_C5, NOTE_C5, NOTE_G5, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_G5,
+//  NOTE_F5, NOTE_F5, NOTE_E5, NOTE_E5, NOTE_D5, NOTE_D5, NOTE_C5,
+//  NOTE_G5, NOTE_G5, NOTE_F5, NOTE_F5, NOTE_E5, NOTE_E5, NOTE_D5,
+//  NOTE_G5, NOTE_G5, NOTE_F5, NOTE_F5, NOTE_E5, NOTE_E5, NOTE_D5,
+//  NOTE_C5, NOTE_C5, NOTE_G5, NOTE_G5, NOTE_A5, NOTE_A5, NOTE_G5,
+//  NOTE_F5, NOTE_F5, NOTE_E5, NOTE_E5, NOTE_D5, NOTE_D5, NOTE_C5,
+//};//
+int melody[] = {
+  NOTE_G4 , NOTE_C5,NOTE_D5, NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+  NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_C5, NOTE_B4,
+  NOTE_B4, NOTE_G4 , NOTE_C5,NOTE_D5, NOTE_E5, NOTE_G5, NOTE_C5,
+  NOTE_D5,NOTE_E5, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_C5, 
+  NOTE_B4,NOTE_B4,
+};
+int noteDurations[] = {
+  8, 8, 8, 2, 2, 2, 8,
+  2, 4, 4, 4, 4, 2, 8,
+  8,  8, 8, 8, 2, 2, 2, 8,
+  2, 4, 4, 4, 4, 2, 8,
+  8
+};
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(tonePin, channel);
+}
+
+void loop() {
+    for (int thisNote = 0; thisNote < 30; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      ledcWriteTone(channel, melody[thisNote]);
+      delay(noteDuration);
+      ledcWriteTone(channel, 0);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+}
+
